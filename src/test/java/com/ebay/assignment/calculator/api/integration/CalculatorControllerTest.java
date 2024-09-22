a431c34d1153a7ce507b18db0a8985d65a0ca337
Anna Pitaev <anna.pitaev@gmail.com>
2024-09-22 14:39:23 -0700

    Added BadRequest tests for the api level

--- src/test/java/com/ebay/assignment/calculator/api/integration/CalculatorControllerTest.java
+++ src/test/java/com/ebay/assignment/calculator/api/integration/CalculatorControllerTest.java
@@ -29,10 +29,24 @@ public class CalculatorControllerTest {
     }
 
     @Test
+    public void getAddWithBadRequest() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/api/calculator/add?firstSummand=5&secondSummand=1", String.class);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertThat(response.getBody()).contains("Bad Request");
+    }
+
+    @Test
     public void getSubtract() throws Exception {
         ResponseEntity<String> response = template.getForEntity("/api/calculator/subtract?minuend=5&subtrahend=1&argumentsType=INTEGER", String.class);
         assertTrue(response.getStatusCode().is2xxSuccessful());
         assertThat(response.getBody()).isEqualTo("4");
+    }
+
+    @Test
+    public void getSubtractWithBadRequest() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/api/calculator/subtract?minuend=5&argumentsType=INTEGER", String.class);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertThat(response.getBody()).contains("Bad Request");
     }
 
     @Test
@@ -43,9 +57,23 @@ public class CalculatorControllerTest {
     }
 
     @Test
+    public void getMultiplyWithBadRequest() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/api/calculator/multiply?multiplier=10&argumentsType=INTEGER", String.class);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertThat(response.getBody()).contains("Bad Request");
+    }
+
+    @Test
     public void getDivide() throws Exception {
         ResponseEntity<String> response = template.getForEntity("/api/calculator/divide?dividend=5&divisor=2&argumentsType=INTEGER", String.class);
         assertTrue(response.getStatusCode().is2xxSuccessful());
         assertThat(response.getBody()).isEqualTo("2");
+    }
+
+    @Test
+    public void getDivideWithBadRequest() throws Exception {
+        ResponseEntity<String> response = template.getForEntity("/api/calculator/divide?dividend=5&divisor=2", String.class);
+        assertTrue(response.getStatusCode().is4xxClientError());
+        assertThat(response.getBody()).contains("Bad Request");
     }
 }
